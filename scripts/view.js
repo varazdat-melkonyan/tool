477bd1362c955990291e0f4b064dfbd84f741a15
varazdat-melkonyan <varazdat.melkonyan@tumo.org>
2021-11-29 17:41:57 +0400

    Update view.js

--- scripts/view.js
+++ scripts/view.js
@@ -1,13 +1,27 @@
 let set = { index: 0 };
 
-$.getJSON( "ajax/test.json", function( data ) {
-    var items = [];
-    $.each( data, function( key, val ) {
-      items.push( "<li id='" + key + "'>" + val + "</li>" );
-    });
-   
-    $( "<ul/>", {
-      "class": "my-new-list",
-      html: items.join( "" )
-    }).appendTo( "body" );
+const onLoad = fetch('data/data.json')
+  .then(response => response.json())
+  .then(data => {
+    set.data = data.data;
+    for(let i = 0; i < set.data.left.length; i++) {
+      view.createLeftWord(i, set.data.left[i].text);
+      
+      view.scrollWords(i, set.data.left[i].text);
+    }
+    for(let j = 0; j < set.data.right.length; j++) {
+      view.createRightWord(j, set.data.right[j].text);
+    }
   });
+
+function checkStatus() {
+
+}
+
+$("#divOne").scroll(function() {
+  for(let i = 0; i < set.data.left.length; i++) {
+    $(`${i}`).css("margin-top", i - 4)
+  }
+})
+
+$(onLoad)
