1fad6d3039cece070e5aa179bd3cf850bb2729bc
varazdat-melkonyan <varazdat.melkonyan@tumo.org>
2021-11-29 16:39:55 +0400

    Create view.js

---
+++ scripts/view.js
@@ -0,0 +1,13 @@
+let set = { index: 0 };
+
+$.getJSON( "ajax/test.json", function( data ) {
+    var items = [];
+    $.each( data, function( key, val ) {
+      items.push( "<li id='" + key + "'>" + val + "</li>" );
+    });
+   
+    $( "<ul/>", {
+      "class": "my-new-list",
+      html: items.join( "" )
+    }).appendTo( "body" );
+  });
