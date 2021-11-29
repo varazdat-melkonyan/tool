1ae11b8dbc59981feba80df1fd242935a7c18593
varazdat-melkonyan <varazdat.melkonyan@tumo.org>
2021-11-29 19:32:35 +0400

    Update view.js

--- scripts/view.js
+++ scripts/view.js
@@ -1,17 +1,19 @@
-let set = { index: 0 };
+let set = { };
+
 
 const onLoad = fetch('data/data.json')
   .then(response => response.json())
   .then(data => {
     set.data = data.data;
+    let length = set.data.left.length;
 
-    view.createLeftWord(0, set.data.left[0].text);
-    view.createLeftWord(1, set.data.left[1].text);
-    view.createLeftWord(2, set.data.left[2].text);
+    view.createLeftWord(0, set.data.left[Math.floor(Math.random() * length)].text);
+    view.createLeftWord(1, set.data.left[Math.floor(Math.random() * length)].text);
+    view.createLeftWord(2, set.data.left[Math.floor(Math.random() * length)].text);
 
-    view.createRightWord(0, set.data.right[0].text);
-    view.createRightWord(1, set.data.right[1].text);
-    view.createRightWord(2, set.data.right[2].text);
+    view.createRightWord(0, set.data.right[Math.floor(Math.random() * length)].text);
+    view.createRightWord(1, set.data.right[Math.floor(Math.random() * length)].text);
+    view.createRightWord(2, set.data.right[Math.floor(Math.random() * length)].text);
   });
 
 function checkStatus() {
@@ -19,15 +21,14 @@ function checkStatus() {
   $("#tw1").animate({"right": -200}, 32);
 
   setTimeout(() => {
-    $("#1").remove();
+    view.removeWord(1);
     $("#tw1").remove();
     $("#0").animate({"top": 60}, 32);
     $("#tw0").animate({"top": 60}, 32);
   }, 333);
+
+  view.editWord();
 }
 
-$("#divOne").scroll(function() {
 
-})
-
-$(onLoad)
+$(onLoad);
