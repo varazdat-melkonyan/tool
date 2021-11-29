48483796d56b3abfb2991c4fb1ca12eaf478072b
varazdat-melkonyan <varazdat.melkonyan@tumo.org>
2021-11-29 18:31:15 +0400

    Update view.js

--- scripts/view.js
+++ scripts/view.js
@@ -4,24 +4,30 @@ const onLoad = fetch('data/data.json')
   .then(response => response.json())
   .then(data => {
     set.data = data.data;
-    for(let i = 0; i < set.data.left.length; i++) {
-      view.createLeftWord(i, set.data.left[i].text);
-      
-      view.scrollWords(i, set.data.left[i].text);
-    }
-    for(let j = 0; j < set.data.right.length; j++) {
-      view.createRightWord(j, set.data.right[j].text);
-    }
+
+    view.createLeftWord(0, set.data.left[0].text);
+    view.createLeftWord(1, set.data.left[1].text);
+    view.createLeftWord(2, set.data.left[2].text);
+
+    view.createRightWord(0, set.data.right[0].text);
+    view.createRightWord(1, set.data.right[1].text);
+    view.createRightWord(2, set.data.right[2].text);
   });
 
 function checkStatus() {
+  $("#1").animate({"left": -200}, 32);
+  $("#tw1").animate({"right": -200}, 32);
 
+  setTimeout(() => {
+    $("#1").remove();
+    $("#tw1").remove();
+    $("#0").animate({"top": 60}, 32);
+    $("#tw0").animate({"top": 60}, 32);
+  }, 333);
 }
 
 $("#divOne").scroll(function() {
-  for(let i = 0; i < set.data.left.length; i++) {
-    $(`${i}`).css("margin-top", i - 4)
-  }
+
 })
 
 $(onLoad)
