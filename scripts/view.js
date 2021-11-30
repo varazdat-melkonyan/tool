474e66552254f05ecd82d1fff19d6f4b0d98d890
varazdat-melkonyan <varazdat.melkonyan@tumo.org>
2021-11-30 16:58:35 +0400

    Update view.js

--- scripts/view.js
+++ scripts/view.js
@@ -1,45 +1,45 @@
 const view = {
-  createWord: (i, text, type, parent) => {
-      let htmlWord = `
-          <div id="${i}" class="word ${type}">
-              <p class="text">${text}</p>
-          </div>
-      `;
-      $(`#${parent}`).append(htmlWord);
+    createWord: (i, text, type, parent) => {
+        let htmlWord = `
+            <div id="${i}" class="word ${type}">
+                <p class="text">${text}</p>
+            </div>
+        `;
+        $(`#${parent}`).append(htmlWord);
 
-      $(`#${parent} #${i}`).css("margin-top", 120 * i);
-  },
-  editWord: (i, text) => {
-      $(`#${i} .text`).html(text);
-  },
-  removeWord: (i) => {
-      $(`#${i} .text`).remove();
-  },
-  scrollToWord: async(direction) => {
-    if (direction > 0) {
-      let count = -1;
-      for(let i = 0; i < 10; i++)
-      {
-          $(`#divOne #${i}`).css("margin-top", 120 * count);
-          count++;  
+        $(`#${parent} #${i}`).css("margin-top", 120 * i);
+    },
+    editWord: (i, text) => {
+        $(`#${i} .text`).html(text);
+    },
+    removeWord: (i) => {
+        $(`#${i} .text`).remove();
+    },
+    scrollToWord: async(direction) => {
+      if (direction > 0) {
+        let count = -1;
+        for(let i = 0; i < 10; i++)
+        {
+            $(`#divOne #${i}`).css("margin-top", 120 * count);
+            count++;
+        }
       }
+
+      $("#divOne .word").removeClass("top center bottom");
+      $(`#divOne #${set.index + 1}`).addClass("center");
+
+      view.editWord(set.index + 1, set.data.left[set.index + 1].text);
+
+      if (direction > 0) {
+          $(`#divOne #${set.index}`).addClass("top");
+          $(`#divOne #${set.index - 1}`).addClass("bottom");
+          $(`#divOne #${set.index - 1}`).hide();
+          $(`#divOne #${set.index - 1}`).css("margin-top", "240px");
+          $(`#divOne #${set.index + 1}`).css("margin-top", "120px");
+          $(`#divOne #${set.index}`).css("margin-top", "0");
+
+          $(`#divOne #${set.index - 1}`).attr("id", set.index + 2);
+          setTimeout(() => $(`#${set.index + 2}`).show());
+      } 
     }
-
-    $("#divOne .word").removeClass("top center bottom");
-    $(`#divOne #${set.index + 1}`).addClass("center");
-
-    view.editWord(set.index + 1, set.data.left[set.index + 1].text);
-
-    if (direction > 0) {
-        $(`#divOne #${set.index}`).addClass("top");
-        $(`#divOne #${set.index - 1}`).addClass("bottom");
-        $(`#divOne #${set.index - 1}`).hide();
-        $(`#divOne #${set.index - 1}`).css("margin-top", "240px");
-        $(`#divOne #${set.index + 1}`).css("margin-top", "120px");
-        $(`#divOne #${set.index}`).css("margin-top", "0");
-
-        $(`#divOne #${set.index - 1}`).attr("id", set.index + 2);
-        setTimeout(() => $(`#${set.index + 2}`).show());
-    } 
-  }
 }
