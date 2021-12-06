1a6c80773e57ec5646a205de28a12ef3265dee25
varazdat-melkonyan <94180926+varazdat-melkonyan@users.noreply.github.com>
2021-12-06 10:54:13 +0400

    Update view.js

--- scripts/view.js
+++ scripts/view.js
@@ -16,30 +16,8 @@ const view = {
         $(`#${i} .text`).remove();
     },
     scrollToWord: async(direction, obj) => {
-      if (direction > 0) {
-        let count = -1;
-        for(let i = 0; i < 10; i++)
-        {
-            $(`#divOne #${i}`).css("margin-top", 120 * count);
-            count++;
-        }
-      }
-
-      $("#divOne .word").removeClass("top center bottom");
-      $(`#divOne #${set.index + 1}`).addClass("center");
-
-      view.editWord(set.index + 1, set.data.left[set.index + 1].text);
-      if (direction > 0) {
-          $(`#divOne #${set.index}`).addClass("top");
-          $(`#divOne #${set.index - 1}`).addClass("bottom");
-          $(`#divOne #${set.index - 1}`).hide();
-          $(`#divOne #${set.index - 1}`).css("margin-top", "240px");
-          $(`#divOne #${set.index + 1}`).css("margin-top", "120px");
-          $(`#divOne #${set.index}`).css("margin-top", "0");
-
-          $(`#divOne #${set.index - 1}`).attr("id", set.index + 2);
-          setTimeout(() => $(`#divOne #${set.index + 2}`).show());
-      } 
+      let offset = direction > 0 ? "+=77" : "-=77";
+        $(".divs").css("top", offset);
     }
 }
  
